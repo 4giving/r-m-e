aabff1473b19d222761c6cf753868dbfbc116b9c
Pete Carr <petrogad@gmail.com>
2022-06-03 13:45:26 -0500

    mobile bar scroll

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,5 @@
 import debounce from 'lodash/debounce';
-import * as React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import Editor from '../../src';
@@ -27,116 +27,130 @@ class YoutubeEmbed extends React.Component {
 	}
 }
 
-class Example extends React.Component {
-	state = {
-		readOnly: false,
-		template: false,
-		dark: localStorage.getItem('dark') === 'enabled',
-		value: undefined
+const Example = () => {
+	const [readOnly, setReadOnly] = React.useState(false);
+	const [template, setTemplate] = useState(false);
+	const [dark, setDarkMode] = useState(localStorage.getItem('dark') === 'enabled');
+	const [value, setValue] = useState(undefined);
+	const [imageUrl, setImageUrl] = useState();
+	const [imageHandler, setHandler] = useState();
+
+	const handleToggleReadOnly = () => {
+		setReadOnly(!readOnly);
 	};
 
-	handleToggleReadOnly = () => {
-		this.setState({ readOnly: !this.state.readOnly });
+	const handleToggleTemplate = () => {
+		setTemplate(!template);
 	};
 
-	handleToggleTemplate = () => {
-		this.setState({ template: !this.state.template });
-	};
-
-	handleToggleDark = () => {
-		const dark = !this.state.dark;
-		this.setState({ dark });
+	const handleToggleDark = () => {
+		setDarkMode({ dark: !dark });
 		localStorage.setItem('dark', dark ? 'enabled' : 'disabled');
 	};
 
-	handleUpdateValue = () => {
+	const handleUpdateValue = () => {
 		const existing = localStorage.getItem('saved') || '';
-		const value = `${existing}\n\nedit!`;
-		localStorage.setItem('saved', value);
+		const updatedValue = `${existing}\n\nedit!`;
+		localStorage.setItem('saved', updatedValue);
 
-		this.setState({ value });
+		setValue(updatedValue);
 	};
 
-	handleChange = debounce(value => {
+	const handleChange = debounce(value => {
 		const text = value();
 		localStorage.setItem('saved', text);
 	}, 250);
 
-	render() {
-		const { body } = document;
-		if (body) body.style.backgroundColor = this.state.dark ? '#181A1B' : '#FFF';
+	const { body } = document;
+	if (body) body.style.backgroundColor = dark ? '#181A1B' : '#FFF';
 
-		return (
+	const handleChangeOfInput = e => {
+		setImageUrl(e.target.value);
+	};
+
+	const callImageAdd = () => {
+		if (imageUrl && imageHandler) {
+			imageHandler(imageUrl);
+		} else {
+			console.log('imageUrl', imageUrl);
+			console.log('imageHandler', imageHandler);
+		}
+	};
+
+	return (
+		<div>
 			<div>
-				<div>
-					<br />
-					<button type="button" onClick={this.handleToggleReadOnly}>
-						{this.state.readOnly ? 'Switch to Editable' : 'Switch to Read-only'}
-					</button>{' '}
-					<button type="button" onClick={this.handleToggleDark}>
-						{this.state.dark ? 'Switch to Light' : 'Switch to Dark'}
-					</button>{' '}
-					<button type="button" onClick={this.handleToggleTemplate}>
-						{this.state.template ? 'Switch to Document' : 'Switch to Template'}
-					</button>{' '}
-					<button type="button" onClick={this.handleUpdateValue}>
-						Update value
-					</button>
-				</div>
-				<br />
-				<br />
-				<Editor
-					id="example"
-					readOnly={this.state.readOnly}
-					readOnlyWriteCheckboxes
-					value={this.state.value}
-					template={this.state.template}
-					defaultValue={defaultValue}
-					scrollTo={window.location.hash}
-					handleDOMEvents={{
-						focus: () => console.log('FOCUS'),
-						blur: () => console.log('BLUR'),
-						paste: () => console.log('PASTE'),
-						touchstart: () => console.log('TOUCH START')
-					}}
-					onSave={options => console.log('Save triggered', options)}
-					onCancel={() => console.log('Cancel triggered')}
-					onChange={this.handleChange}
-					// onClickLink={(href, event) => console.log('Clicked link: ', href, event)}
-					onHoverLink={event => {
-						console.log('Hovered link: ', event.target.href);
-						return false;
-					}}
-					onShowToast={(message, type) => window.alert(`${type}: ${message}`)}
-					insertImageHandler={handler => {
-						handler('https://picsum.photos/600/600');
-					}}
-					embeds={[
-						{
-							title: 'YouTube',
-							keywords: 'youtube video tube google',
-							icon: () => (
-								<img
-									src="https://upload.wikimedia.org/wikipedia/commons/7/75/YouTube_social_white_squircle_%282017%29.svg"
-									width={24}
-									height={24}
-								/>
-							),
-							matcher: url => {
-								return url.match(
-									/(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})$/i
-								);
-							},
-							component: YoutubeEmbed
-						}
-					]}
-					dark={this.state.dark}
-					autoFocus
-				/>
+				<input onChange={handleChangeOfInput} onBlur={callImageAdd}></input>
 			</div>
-		);
-	}
-}
+			<div>
+				<br />
+				<button type="button" onClick={handleToggleReadOnly}>
+					{readOnly ? 'Switch to Editable' : 'Switch to Read-only'}
+				</button>{' '}
+				<button type="button" onClick={handleToggleDark}>
+					{dark ? 'Switch to Light' : 'Switch to Dark'}
+				</button>{' '}
+				<button type="button" onClick={handleToggleTemplate}>
+					{template ? 'Switch to Document' : 'Switch to Template'}
+				</button>{' '}
+				<button type="button" onClick={handleUpdateValue}>
+					Update value
+				</button>
+			</div>
+			<br />
+			<br />
+			<Editor
+				id="example"
+				readOnly={readOnly}
+				readOnlyWriteCheckboxes
+				value={value}
+				template={template}
+				defaultValue={defaultValue}
+				scrollTo={window.location.hash}
+				handleDOMEvents={{
+					focus: () => console.log('FOCUS'),
+					blur: () => console.log('BLUR'),
+					paste: () => console.log('PASTE'),
+					touchstart: () => console.log('TOUCH START')
+				}}
+				onSave={options => console.log('Save triggered', options)}
+				onCancel={() => console.log('Cancel triggered')}
+				onChange={handleChange}
+				// onClickLink={(href, event) => console.log('Clicked link: ', href, event)}
+				onHoverLink={event => {
+					console.log('Hovered link: ', event.target.href);
+					return false;
+				}}
+				onShowToast={(message, type) => window.alert(`${type}: ${message}`)}
+				insertImageHandler={handler => {
+					setHandler(() => handler);
+					// handler('https://picsum.photos/600/600');
+				}}
+				embeds={[
+					{
+						title: 'YouTube',
+						keywords: 'youtube video tube google',
+						icon: () => (
+							<img
+								src="https://upload.wikimedia.org/wikipedia/commons/7/75/YouTube_social_white_squircle_%282017%29.svg"
+								width={24}
+								height={24}
+							/>
+						),
+						matcher: url => {
+							return url.match(
+								/(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})$/i
+							);
+						},
+						component: YoutubeEmbed
+					}
+				]}
+				dark={dark}
+				autoFocus
+			/>
+		</div>
+	);
+};
 
 if (element) {
 	ReactDOM.render(<Example />, element);
